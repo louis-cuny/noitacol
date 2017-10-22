6b1877a49b4b98a9289385f0504d82f9c31e381b
LouisCuny <louis-cuny@orange.fr>
2017-10-22 20:08:39 +0200

    Perfect

--- src/App/Resources/assets/js/app.js
+++ src/App/Resources/assets/js/app.js
@@ -1,8 +1,10 @@
 $(function () {
-    /*$('.message, .close').delay(4000).fadeOut();*/
-    $('.message, .close').delay(2000).animate({
-        top: "-=50"},function() {
-        $(this).remove();
-    })
-});
 
+    //This is for hidding the flash messages
+    $('.message').each(function () {
+        $(this).delay(3000).animate({'margin-bottom': 0, 'margin-top': 0}, 2000);
+        $('body').delay(3000).animate({'padding-top': '-=50px'}, 2000);
+    });
+
+
+});
